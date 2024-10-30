af36a93480fb8e6a9813beb23cf266b897884150
Petar Ceho <petar.ceho@peoplefone.com>
2024-10-30 21:51:38 +0100

    startet lstm implementation

---
+++ src/lstm.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class LSTM: #TODO: ...
+    
+    def __init__(self,n_letters,hidden_size):
+        pass
+
+    def forward():
+        pass
+
+    def backward():
+        pass
+
