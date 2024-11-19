308249a193cc271c9f723d3f83b2d0a1346031a5
Petar Ceho <petarceho@gmail.com>
2024-11-19 18:48:05 +0100

    autoencoder forward pass

--- src/rnn.py
+++ src/rnn.py
@@ -135,8 +135,33 @@ class LSTM:
     def sigmoid(self,x):
         return 1 / (1 + np.exp(-x))
     
+class Autoencoder:
+    def __init__(self, input_size, hidden_size, learning_rate=0.001):
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.learning_rate = learning_rate
+        
+        self.W1 = np.random.randn(input_size, hidden_size) * 0.01
+        self.b1 = np.zeros((1, hidden_size))
+        self.W2 = np.random.randn(hidden_size, input_size) * 0.01
+        self.b2 = np.zeros((1, input_size))
 
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
 
+    def forward(self,X):
+        #encoder 
+        self.z1=np.dot(X,self.W1)+self.b1
+        self.a1=self.sigmoid(self.z1)
+
+        #decoder 
+        self.z2=np.dot(self.a1,self.W2)+self.b2
+        self.a2=self.sigmoid(self.z2)
+
+        return self.a2
+    
+    def backward(self):
+        pass
 
 #softmax+categorical cross entropy 
 class CrossEntropyLoss: 
